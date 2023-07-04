1d26d46501bb9e9cef623ca2d969eb2a565c909e
Winnard Arthur <winnardarthur7@gmail.com>
2023-07-04 18:21:00 +0000

    make as reusable components

---
+++ lib/widgets/header_row.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_booking_app/utils/app_layout.dart';
+import 'package:gap/gap.dart';
+import 'package:ticket_booking_app/utils/app_styles.dart';
+
+class HeaderRow extends StatelessWidget {
+  final String largeText;
+  final String smallText;
+  const HeaderRow(
+      {super.key, required this.largeText, required this.smallText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(largeText, style: Styles.headlineStyle2),
+        InkWell(
+          onTap: () {},
+          child: Text(smallText,
+              style: Styles.textStyle.copyWith(color: Styles.primaryColor)),
+        )
+      ],
+    );
+  }
+}
